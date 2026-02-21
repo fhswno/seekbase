363c95ab8ad493d3c7e786388a24e2331fefffbe
David <66641216+fhswno@users.noreply.github.com>
2026-02-21 02:48:12 +0000

    types

---
+++ src/types/ai.ts
@@ -0,0 +1,15 @@
+export type AIAction = "summarize" | "rewrite" | "explain" | "translate";
+
+export const AI_LANGUAGES = [
+  "Spanish",
+  "French",
+  "German",
+  "Japanese",
+  "Chinese",
+  "Korean",
+  "Portuguese",
+  "Italian",
+  "Russian",
+  "Hebrew",
+  "Arabic",
+];
