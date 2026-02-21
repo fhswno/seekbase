ca5abb0f752bee3d39076cd7b355f3b67a8e8ff0
David <66641216+fhswno@users.noreply.github.com>
2026-02-21 02:54:08 +0000

    TYPES

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type { Workspace } from "./workspace";
+export type { Page, CreatePageParams, UpdatePageParams, DatabaseViewType } from "./page";
+export type { Block, BlockType, CreateBlockParams, BlockReorder } from "./block";
+export type {
+  DatabaseProperty,
+  DatabaseRow,
+  DatabaseCell,
+  DatabaseView,
+  PropertyType,
+} from "./database";
+export type { SettingKey } from "./settings";
+export type { SearchResult } from "./search";
